ade132626702b8c1b3571d485b5bc6f0779a7279
lukedeo <lukepercivaldeoliveira@gmail.com>
2015-07-07 16:29:50 +0200

    formatting for gh

--- dl/deepjets.py
+++ dl/deepjets.py
@@ -21,55 +21,55 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class DeepNet(object):
-	'''
-	A wrapper class around Keras models -- allows activation inspection.
-	'''
-	def __init__(self, pretraining = True, model=Sequential()):
-		super(DeepNet, self).__init__()
-		self.model = model
-		self.pretraining = pretraining
+    '''
+    A wrapper class around Keras models -- allows activation inspection.
+    '''
+    def __init__(self, pretraining = True, model=Sequential()):
+        super(DeepNet, self).__init__()
+        self.model = model
+        self.pretraining = pretraining
 
-		self._is_pretrained = False
+        self._is_pretrained = False
 
-		self._is_compiled = []
+        self._is_compiled = []
 
-		self.submodels = {}
+        self.submodels = {}
 
-		self.autoencoder = []
+        self.autoencoder = []
 
-		self._model_cache = []
+        self._model_cache = []
 
-		self.weights = None
+        self.weights = None
 
-		self.callbacks = {}
+        self.callbacks = {}
 
-	def add(layer, **kwargs):
-		'''
-		Functionality to add a Keras layer to a DeepNet
-		'''
-		loss, optimizer = None, None
-		if 'loss' not in kwargs.keys():
-			loss = 'mse'
-		if 'optimizer' not in kwargs.keys():
-			optimizer = Adam()
-		logger.info('Adding {} layer'.format(layer.__class__))
-		if type(layer) == AutoEncoder and not self.pretraining:
-				raise TypeError(
-					'pretraining = False set while trying to add an AutoEncoder'
-					)
-		else:
-			# self._is_compiled.append(False)
-			self._model_cache.append(Sequential())
-			_model_cache[-1].add(layer)
-			autoencoder[-1].compile(loss=loss, optimizer=optimizer)
-
-
-	# def pretrain(X, **kwargs):
-	# 	pass
+    def add(layer, **kwargs):
+        '''
+        Functionality to add a Keras layer to a DeepNet
+        '''
+        loss, optimizer = None, None
+        if 'loss' not in kwargs.keys():
+            loss = 'mse'
+        if 'optimizer' not in kwargs.keys():
+            optimizer = Adam()
+        logger.info('Adding {} layer'.format(layer.__class__))
+        if type(layer) == AutoEncoder and not self.pretraining:
+                raise TypeError(
+                    'pretraining = False set while trying to add an AutoEncoder'
+                    )
+        else:
+            # self._is_compiled.append(False)
+            self._model_cache.append(Sequential())
+            _model_cache[-1].add(layer)
+            autoencoder[-1].compile(loss=loss, optimizer=optimizer)
 
 
+    # def pretrain(X, **kwargs):
+    #   pass
 
 
 
 
-		
+
+
+        
--- utils/sampling.py
+++ utils/sampling.py
@@ -45,46 +45,46 @@ def multinomial_sample(p, k=1):
 
 
 class WeightedDataset(object):
-	"""docstring for WeightedDataset"""
-	def __init__(self, X, y=None, weights=None, copy = True):
-		if copy:
-			self._X = X.copy()
-			if not y is None:
-				self._y = y.copy()
-			else:
-				self._y = None
-		else:
-			self._X = X
-			self._y = y
+    """docstring for WeightedDataset"""
+    def __init__(self, X, y=None, weights=None, copy = True):
+        if copy:
+            self._X = X.copy()
+            if not y is None:
+                self._y = y.copy()
+            else:
+                self._y = None
+        else:
+            self._X = X
+            self._y = y
 
-		if weights is None:
-			weights = np.ones(X.shape[0])
+        if weights is None:
+            weights = np.ones(X.shape[0])
 
 
-		if not type(weights) in [np.array, list, np.ndarray]:
-			raise TypeError('weights must be a numpy array or a list')
+        if not type(weights) in [np.array, list, np.ndarray]:
+            raise TypeError('weights must be a numpy array or a list')
 
-		if len(weights) != X.shape[0]:
-			raise ValueError('weights must have the same length as the first axis of X')
-		
-		if type(weights) is list:
-			self._weights = np.array(weights)
-		else:
-			self._weights = weights
+        if len(weights) != X.shape[0]:
+            raise ValueError('weights must have the same length as the first axis of X')
+        
+        if type(weights) is list:
+            self._weights = np.array(weights)
+        else:
+            self._weights = weights
 
-		self._weights[self._weights < 0] = 0.0
+        self._weights[self._weights < 0] = 0.0
 
-		self._weights = self._weights / np.sum(self._weights)
+        self._weights = self._weights / np.sum(self._weights)
 
-	def sample_idx(self, n):
-		return multinomial_sample(self._weights, n)
+    def sample_idx(self, n):
+        return multinomial_sample(self._weights, n)
 
-	def sample(self, n):
-		ix = multinomial_sample(self._weights, n)
-		if self._y is None:
-			return self._X[ix]
-		else:
-			return self._X[ix], self._y[ix]
+    def sample(self, n):
+        ix = multinomial_sample(self._weights, n)
+        if self._y is None:
+            return self._X[ix]
+        else:
+            return self._X[ix], self._y[ix]
 
 
 
